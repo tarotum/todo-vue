28b3da8c9e1d60096c372c77ee4488d2cc13ca31
tarotum <onyskiv.maxim@gmail.com>
2019-04-27 15:12:47 +0300

    Add sorting by all/active/completed

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,9 +4,9 @@
       <TodoItem v-for="todo in todos" :key="todo._id" :todo="todo"/>
     </div>
     <div class="todo-filters">
-      <button>All</button>
-      <button>Active</button>
-      <button>Completed</button>
+      <button @click="filterTodos('all')">All</button>
+      <button @click="filterTodos('active')">Active</button>
+      <button @click="filterTodos('complited')">Completed</button>
     </div>
   </div>
 </template>
@@ -18,23 +18,51 @@ export default {
   components: {
     TodoItem
   },
-  computed: {
-    todos() {
-      return this.$store.getters["Todos/getTodos"];
-    }
+  data() {
+    return {
+      filter: "all"
+    };
   },
   created() {
     this.$store.dispatch("Todos/GET_TODOS");
+  },
+  computed: {
+    todos() {
+      switch (this.filter) {
+        case "all":
+          return this.$store.getters["Todos/getTodos"];
+        case "active":
+          return this.$store.getters["Todos/getTodos"].filter(
+            todo => todo.completed === false
+          );
+        case "complited":
+          return this.$store.getters["Todos/getTodos"].filter(
+            todo => todo.completed === true
+          );
+        default:
+          return this.$store.getters["Todos/getTodos"];
+      }
+    }
+  },
+  methods: {
+    filterTodos(filter) {
+      this.filter = filter;
+    }
   }
 };
 </script>
 <style scoped>
 .todo-list {
   grid-area: TodoList;
-  overflow-y: scroll;
+  overflow-y: auto;
   max-height: 70vh;
   margin-top: 5em;
-  padding-right: 1em;
+}
+
+@media (min-width: 1100px) {
+  .todo-list {
+    padding-right: 1em;
+  }
 }
 </style>
 
